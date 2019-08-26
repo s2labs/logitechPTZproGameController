a9ad9386f0308084cc9292d56464197e064373fb
Marc-Oliver Pahl <pahl@tum.de>
2019-08-26 19:32:38 +0200

    Cleaned up and finished implementing what I started with you before. Put comments (to be kept) about the implementation.

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import evdev
-from camcontrol import execute
 import scale
 from subprocess import check_output
 import configparser
 
 def uvcSET(command, value):
     check_output("uvcdynctrl -d {} -s \'{}\' {}".format(cam_name, command, value), shell=True)
+    print("uvcdynctrl -d {} -s \'{}\' {}".format(cam_name, command, value))
 def uvcGET(command, param):
     check_output("uvcdynctrl -d {} -g \'{}\'".format(cam_name, command), shell=True)
+    print("uvcdynctrl -d {} -g \'{}\'".format(cam_name, command))
 
 def gamepad_control(device, cam_name):
 
-    # isPaningRight = False
-    # isPaningLeft = False
-    # isTiltingUp = False
-    # isTiltingDown = False
+    # for storing where the camera is currently moving not to issue double
     movingDirection = [0, 0]
 
     for ev in device.read_loop():
@@ -22,18 +20,35 @@ def gamepad_control(device, cam_name):
             res = evdev.categorize(ev)
             res = str(res)
             res = res.split(",")[1].split("(")[0].strip()
+
+            ### Movement (Pan/ Tilt)
+            # C=right
             if res == "305":
-                ops = execute("right", cam_name)
+                uvcSET("Pan (Absolute)", "1")
+            # A=left
             elif res == "307":
-                ops = execute("left", cam_name)
+                uvcSET("Pan (Absolute)", "-- -1")
+            # B=UP
             elif res == "308":
-                ops = execute("up", cam_name)
+                uvcSET("Tilt (Absolute)", "1") 
+            # D=down
             elif res == "304":
-                ops = execute("down", cam_name)
+                uvcSET("Tilt (Absolute)", "-- -1")
+
+            ### Other functions
+            # select: toggle other commands (zoom, exposure, ...) for usage withthe back buttons
+            # start: select between auto and manual
+
+            # back right button = +1 for the selected property
+            # back left button = -1 for the selected property
+
+            # left cross: up/ down = Zoom one step;
+            # left cross: left/right = exposure one step
             else:
                 ops = True
                 continue
         else:
+        # "analog" buttons
             res = repr(ev)
             res = str(res)
             res = res.split("InputEvents")
@@ -44,7 +59,7 @@ def gamepad_control(device, cam_name):
                 sarg = int(r[4])
                 #print("{}-{}".format(farg, sarg))
                 # farg = 3 : right controller and side movement
-
+                # farg = 4 : right controller and up/down movement
                 if farg in  [3, 4]:
                     should_move = abs(sarg) > 18000
                     is_increasing = (sarg>=0)
@@ -62,45 +77,14 @@ def gamepad_control(device, cam_name):
                         value = "-- -1"
                     if (movingDirection[farg-3] != abs(selectedOperation)):
                         movingDirection[farg-3] = abs(selectedOperation)
-                        print(movingDirection)
                         uvcSET(command2move[farg-3],value)
-                        print("uvcdynctrl -d {} -s \'{}\' {}".format(cam_name,command2move[farg-3],value))
-
-                # if should_move:
-                #     if is_increasing:
-                #
-                #
-                # if float(sarg/32800) > 0.3:
-                #     if farg == 3:
-                #         if not isPaningRight:
-                #             ops = execute("right", cam_name)
-                #             isPaningRight = True
-                #     elif farg == 4:
-                #         if not isTiltingUp:
-                #             ops = execute("up", cam_name)
-                #             isTiltingUp = True
-                # elif float(sarg/32800) < -0.3:
-                #     if farg == 3:
-                #         if not isPaningLeft:
-                #             ops = execute("left", cam_name)
-                #             isPaningLeft = True
-                #     elif farg == 4:
-                #         if not isTiltingDown:
-                #             ops = execute("down", cam_name)
-                #             isTiltingDown = True
-                # else:
-                #     if isPaningRight or isPaningLeft:
-                #         ops = execute("pan_stop", cam_name)
-                #         isPaningRight = False
-                #         isPaningLeft = False
-                #     if isTiltingUp or isTiltingDown:
-                #         ops = execute("tilt_stop", cam_name)
-                #         isTiltingDown = False
-                #         isTiltingUp = False
-
+                # farg= 1 = ZOOM; 0 = exposure
+                # farg = 3 = value selected above - 5= value selected above +
 
 if __name__ == "__main__":
 
+# Change this to work with the command line without config file
+# add a --help -h !
     config = configparser.ConfigParser()
     config.read('config.ini')
     device_name = config['DEVICE']['Name']
